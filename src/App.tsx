f6fa95aa5ad8a069ec49a2bc997b2a7918d5d784
iliya <iliya.yurievich@yandex.ru>
2021-11-11 23:44:15 +0300

    lern ts

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,80 @@ let d3 = d2.map((el) => el / 2);
 let sex: "male" | "female";
 sex = "male";
 
-let user = {
+type AddresType = {
+  city?: string | null;
+  index?: number | null;
+};
+
+type UserType = {
+  sayHello: (name: string) => string;
+  age: number;
+  name: string;
+  isSamurai: boolean;
+  addres: AddresType | null;
+};
+
+let user: UserType = {
   age: 12,
   name: "an",
-  sayHello(name: string){console.log(`Hello, ${name}`);}
+  sayHello(name: string) {
+    return `Hello, ${name}`;
+  },
+  isSamurai: true,
+  addres: {
+    // city: "spb",
+    index: 194,
+  },
+};
+
+let userMethod: string = user.sayHello("mark");
+
+let first;
+first = "string";
+// first = 3;
+
+const summ: (a: number, b: number) => number = (a: number, b: number) => {
+  return a + b;
+};
+
+let testSumm = summ(4, 4);
+
+export type initialStateType = typeof initialState;
+
+let initialState = {
+  name: null as string | null,
+  age: null as number | null,
+  isSamurai: null as boolean | null,
+  addres: {
+    city: null,
+    country: null,
+  } as AddresType,
+  counter: 0,
+  arrayAddreses: [] as Array<AddresType>,
+};
+
+let stateSecond: initialStateType = {
+  addres: {
+    city: "spb",
+  },
+  age: 30,
+  isSamurai: true,
+  name: "i",
+  counter: 5,
+  arrayAddreses: [{ city: "sp", index: 194 }],
+};
+
+let GET_TASK = "api/task";
+let GET_TASKS = "api/task/id";
+
+type actionType = {
+  type: typeof GET_TASKS;
+  payload: number;
+};
+
+let action: actionType = {
+  type: GET_TASKS,
+  payload: 1,
 };
 
 function App() {
@@ -38,6 +108,11 @@ function App() {
       <div>{d3}</div>
       <div>{sex}</div>
       <div>{Object.keys(user)}</div>
+      <div>{userMethod}</div>
+      <div>{testSumm}</div>
+      <div>{summ(5, 5)}</div>
+      <div>{Object.keys(initialState)}</div>
+      <div>{Object.keys(action)}</div>
     </div>
   );
 }
